6a7badb998f0d317c8cecd7773038af224f9c1a1
mikosramek <miko@mikosramek.ca>
2019-12-13 13:09:03 -0500

    Adding fun details

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,35 @@
 const app = {};
 
+app.isFollowerActive = false;
+
+app.bindDomElements = function() {
+  // $('#header-image').on('click', function(){
+  //   $(this).addClass('animate');
+  // })
+  app.$follower = $('#hidden-button');
+  app.$smallFollower = $('#hidden-button-small');
+  $('#egg').on('click', function() {
+    if(!app.isFollowerActive){
+      app.isFollowerActive = true;
+      app.follower();
+    }
+  });
+}
+
+app.follower = function() {
+  app.$follower.css({'z-index':'0'});
+  app.$smallFollower.css({'z-index':'0'});
+  $("html").mousemove(function(event) {
+    app.$follower.css({'top':`${event.pageY - 15}px`, 'left':`${event.pageX - 30}px`});
+    app.$smallFollower.css({'top':`${event.pageY - 10}px`, 'left':`${event.pageX - 10}px`});
+  });
+  app.$follower.addClass('active');
+  app.$smallFollower.addClass('active');
+}
+
 
 app.init = () => {
-  console.log("Document Loaded");
+  app.bindDomElements();
 }
 
 $(document).ready(function(){
